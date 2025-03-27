f7474ab9c781de0a929ebec14307b8ff7da7ca8e
abhilasha3601 <abhilashasharma.0306@gmail.com>
2025-03-27 20:30:38 +0000

    solution

--- src/main/java/com/example/song/SongApplication.java
+++ src/main/java/com/example/song/SongApplication.java
@@ -1,14 +1,14 @@
 
-package com.example.song;
+package com.example.Book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SongApplication {
+public class BookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SongApplication.class, args);
+		SpringApplication.run(BookApplication.class, args);
 	}
 
 }
